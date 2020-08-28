d7b819db0e79577e5e0314fccc57e281107beacc
Phil Austin <paustin@eos.ubc.ca>
2020-08-28 10:36:42 -0700

    extend the pangeo image

--- examples/simple/hub_image/jupyterhub_config.py
+++ examples/simple/hub_image/jupyterhub_config.py
@@ -12,8 +12,8 @@ c.JupyterHub.hub_connect_ip = 'jupyterhub'
 
 # pick a docker image. This should have the same version of jupyterhub
 # in it as our Hub.
-c.DockerSpawner.image = 'pangeo/base-notebook:latest'
-
+c.DockerSpawner.image_whitelist = {'scipy':'jupyter/scipy-notebook:latest',
+                                   'pangeo':'phaustin/notebook:0.1'}
 # tell the user containers to connect to our docker network
 c.DockerSpawner.network_name = 'jupyterhub'
 
