f158155acd8475f76bb3c735c70d2c7127262fd8
Phil Austin <paustin@eos.ubc.ca>
2020-09-23 07:58:10 -0700

    working

--- examples/simple/hub_image/jupyterhub_config.py
+++ examples/simple/hub_image/jupyterhub_config.py
@@ -8,7 +8,7 @@ c.JupyterHub.spawner_class = 'dockerspawner.DockerSpawner'
 c.JupyterHub.hub_ip = '0.0.0.0'
 # the hostname/ip that should be used to connect to the hub
 # this is usually the hub container's name
-c.JupyterHub.hub_connect_ip = 'jupyterhub_test'
+c.JupyterHub.hub_connect_ip = 'jupyterhub_sep23'
 
 # pick a docker image. This should have the same version of jupyterhub
 # in it as our Hub.
@@ -17,14 +17,14 @@ notebook_dir = "/home/jovyan/work"
 c.DockerSpawner.notebook_dir = notebook_dir
 
 # tell the user containers to connect to our docker network
-c.DockerSpawner.network_name = 'net_test'
+c.DockerSpawner.network_name = 'net_sep23'
 c.DockerSpawner.volumes = {"jupyterhub-user-{username}": notebook_dir,
-                            "/Users/phil/repos/a301_2020/sat_data": 
+                            "/ScratchSSD/docker/a301_setup/a301_2020/sat_data": 
                             {"bind": '/home/jovyan/work/sat_data', "mode": "ro"},
-                            "/Users/phil/repos/a301_2020/a301_lib": 
+                            "/ScratchSSD/docker/a301_setup/a301_2020/a301_lib": 
                             {"bind": '/home/jovyan/work/a301_lib', "mode": "ro"},
-                            "/Users/phil/repos/a301_2020/data_share": 
-                            {"bind": '/home/jovyan/work/data_share', "mode": "rw"},
+                            "/ScratchSSD/docker/a301_setup/a301_2020/data_share": 
+                            {"bind": '/home/jovyan/work/data_share', "mode": "rw"}
                            }
 
 
