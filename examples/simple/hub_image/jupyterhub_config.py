794e875e6b189f9bb9c684e7c7b326d4ff0648c8
Phil Austin <you@example.comw>
2020-09-23 06:55:52 -0700

    a301 init

--- examples/simple/hub_image/jupyterhub_config.py
+++ examples/simple/hub_image/jupyterhub_config.py
@@ -12,12 +12,21 @@ c.JupyterHub.hub_connect_ip = 'jupyterhub_test'
 
 # pick a docker image. This should have the same version of jupyterhub
 # in it as our Hub.
-c.DockerSpawner.image = 'phaustin/notebook:step1'
+c.DockerSpawner.image = 'phaustin/notebook:sep23'
 notebook_dir = "/home/jovyan/work"
 c.DockerSpawner.notebook_dir = notebook_dir
 
 # tell the user containers to connect to our docker network
 c.DockerSpawner.network_name = 'net_test'
-c.DockerSpawner.volumes = {"jupyterhub-user-{username}": notebook_dir}
+c.DockerSpawner.volumes = {"jupyterhub-user-{username}": notebook_dir,
+                            "/Users/phil/repos/a301_2020/sat_data": 
+                            {"bind": '/home/jovyan/work/sat_data', "mode": "ro"},
+                            "/Users/phil/repos/a301_2020/a301_lib": 
+                            {"bind": '/home/jovyan/work/a301_lib', "mode": "ro"},
+                            "/Users/phil/repos/a301_2020/data_share": 
+                            {"bind": '/home/jovyan/work/data_share', "mode": "rw"},
+                           }
+
+
 # delete containers when the stop
 c.DockerSpawner.remove = True
