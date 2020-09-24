b1c84ebc2dcaed77f21f52e82b441b5ac589ed95
Phil Austin <paustin@eos.ubc.ca>
2020-09-23 19:39:24 -0700

    n7

--- examples/simple/hub_image/jupyterhub_config.py
+++ examples/simple/hub_image/jupyterhub_config.py
@@ -8,16 +8,16 @@ c.JupyterHub.spawner_class = 'dockerspawner.DockerSpawner'
 c.JupyterHub.hub_ip = '0.0.0.0'
 # the hostname/ip that should be used to connect to the hub
 # this is usually the hub container's name
-c.JupyterHub.hub_connect_ip = 'jupyterhub_sep23'
+c.JupyterHub.hub_connect_ip = 'jupyterhub_sep23n7'
 
 # pick a docker image. This should have the same version of jupyterhub
 # in it as our Hub.
-c.DockerSpawner.image = 'phaustin/notebook:sep23'
+c.DockerSpawner.image = 'phaustin/notebook:sep23n7'
 notebook_dir = "/home/jovyan/work"
 c.DockerSpawner.notebook_dir = notebook_dir
 
 # tell the user containers to connect to our docker network
-c.DockerSpawner.network_name = 'net_sep23'
+c.DockerSpawner.network_name = 'net_sep23n7'
 c.DockerSpawner.volumes = {"jupyterhub-user-{username}": notebook_dir,
                             "/ScratchSSD/docker/a301_setup/a301_2020/sat_data": 
                             {"bind": '/home/jovyan/work/sat_data', "mode": "ro"},
