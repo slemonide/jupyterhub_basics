e2534694b3a27bb9ea22764ee438d30f66f4a699
Danil Platonov <danil.platonov@alumni.ubc.ca>
2020-09-27 13:36:45 +0000

    Added https reverse proxy example with nginx; added simple github oauth example with no whitelist

---
+++ examples/github-oauth/hub_image/jupyterhub_config.py
@@ -0,0 +1,29 @@
+# dummy for testing. Don't use this in production!
+#c.JupyterHub.authenticator_class = 'dummyauthenticator.DummyAuthenticator'
+
+# launch with docker
+c.JupyterHub.spawner_class = 'dockerspawner.DockerSpawner'
+
+# we need the hub to listen on all ips when it is in a container
+c.JupyterHub.hub_ip = '0.0.0.0'
+# the hostname/ip that should be used to connect to the hub
+# this is usually the hub container's name
+c.JupyterHub.hub_connect_ip = 'jupyterhub_basic'
+
+# pick a docker image. This should have the same version of jupyterhub
+# in it as our Hub.
+c.DockerSpawner.image = 'phaustin/notebook:step1'
+notebook_dir = "/home/jovyan/work"
+c.DockerSpawner.notebook_dir = notebook_dir
+
+# tell the user containers to connect to our docker network
+c.DockerSpawner.network_name = 'net_basic'
+c.DockerSpawner.volumes = {"jupyterhub-user-{username}": notebook_dir}
+# delete containers when the stop
+c.DockerSpawner.remove = True
+
+# Github OAuth
+from oauthenticator.github import GitHubOAuthenticator
+c.JupyterHub.authenticator_class = GitHubOAuthenticator
+
+#c.GitLabOAuthenticator.scope = ['openid', 'read_user']
